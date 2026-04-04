a9bd61ef512db2a817506210f5373a7a9f1476b9
Matthew Deline <deline.matt@gmail.com>
2026-04-04 19:49:37 +1100

    Updating maka project image

--- lib/projects/maka.ts
+++ lib/projects/maka.ts
@@ -6,7 +6,7 @@ export const maka: Project = {
   year: "2019",
   role: "Producer, Writer, Designer",
   category: "jam",
-  image: "/images/maka/maka.png",
+  image: "/images/MAKA/avo3-1.gif",
   description: `
 2019 Global Game Jam Entry. A satirical game where you play as a piece of smashed avo, smashing things!
   `,
