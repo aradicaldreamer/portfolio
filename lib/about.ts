f5a5d81539f83e25dacd919b848dc74676b2e2a5
Matthew Deline <deline.matt@gmail.com>
2026-04-06 06:17:30 +1000

    Updating about.ts

--- lib/about.ts
+++ lib/about.ts
@@ -1,17 +1,17 @@
 
 export const tagline = `
-Hi! My name is Matt, and it's very nice to meet you. I'm an Independent Game Creator and Installation Designer originally from California that is currently based in Melbourne. 
+Hi! My name is Matt, and it's very nice to meet you. I'm a Game Creator and Playful Experience Designer originally from California that is currently based in Melbourne. 
 
 I love to travel and make cool things, and I am a passionate believer in the importance of storytelling and the power of creative expression through play. 
 `;
 
-export const bio = `Matt is an experienced programmer, producer, and manager who has been working in the games industry for 7 years. Most recently, he was the producer and lead programmer for the Apple Arcade release of Piffle+, released on iOS, Apple TV, and Mac OS platforms.
+export const bio = `I am an experienced programmer, producer, and manager who has been working in the games industry for 7 years. Most recently, I was the producer and lead programmer for the Apple Arcade release of Piffle+, released on iOS, Apple TV, and Mac OS platforms.
 
-Prior to Piffle+, Matt served as the Head of Automation Engineering at Mighty Build and Test, where he oversaw automated testing, CI/CD pipelines, and release management support for a number of established developers and publishers across mobile, VR, and console platforms. Some notable examples are Star Trek: Lower Decks TBD, Fruit Ninjas 1 and 2, Shooty Skies Overdrive, Armello, and the Nintendo Switch port of Guilty Gear Strive.
+Prior to Piffle+, I served as the Head of Automation Engineering at Mighty Build and Test, where I oversaw automated testing, CI/CD pipelines, and release management support for a number of established developers and publishers across mobile, VR, and console platforms. Some notable examples are Star Trek: Lower Decks TBD, Fruit Ninjas 1 and 2, Shooty Skies Overdrive, Armello, and the Nintendo Switch port of Guilty Gear Strive.
 
-He also created the training and development curriculum and helped facilitate the growth of Mighty Build and Test from a team of five to a team of fifty in a year across multiple global regions. 
+I also created the training and development curriculum and helped facilitate the growth of Mighty Build and Test from a team of five to a team of fifty in a year across multiple global regions. 
 
-Prior to joining the industry, Matt is a veteran in IT support, who worked at both Apple’s Cupertino Headquarters supporting the Worldwide Operations Group with their enterprise needs, and at their flagship store in Palo Alto as an Apple Genius. He is also a graduate of the MA Independent Games and Playable Experience Design program at Goldsmiths in London, best known for his work on the Alternative Controller and VR experiences, “Shape Arcade,” and “Normal for Norman?” `;
+Prior to joining the industry, I was a veteran in IT support, who worked at both Apple's Cupertino Headquarters supporting the Worldwide Operations Group with their enterprise needs, and at their flagship store in Palo Alto as an Apple Genius. I am also a graduate of the MA Independent Games and Playable Experience Design program at Goldsmiths in London, best known for my work on the Alternative Controller and VR experiences, “Shape Arcade,” and “Normal for Norman?” `;
 
 export const skillGroups = [
   {
